9de661416b7dbbf0f8e8f075564fb9e6d0000afb
dmytrodanilenkov <d.danilenkov.ext@sportradar.com>
2024-04-18 15:48:00 +0200

    update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var typewriter = new Typewriter(app, {
 
 typewriter
   .pauseFor(1000)
-  .typeString('I am a software developer and content creator from the United States.') //Add your own tagline
+  .typeString('I am a PR/GR specialist the Ukraine.') //Add your own tagline
   .pauseFor(3000)
   .start()
 
